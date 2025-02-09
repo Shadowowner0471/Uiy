24f05086db08545d7c8a1e247650c9795363c69e
Shadowowner0471 <ha8137764@gmail.com>
2025-02-09 11:41:54 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,19 @@
+# Don't use quotes( " and ' )
+#SCRIPT BY 
+  
+#Enter Your Bot Token here get it from @botfarher
+BOT_TOKEN=("7892020644:AAHA2Soku5kwW9QskpbUWSQTSHklHrGCdM0")
+
+  #Enter Your telegram username here without @
+OWNER_USERNAME=("@SHARP_X_OWNER")
+
+  #Enter your admin id here Get it from @missRose_bot by typing /info
+ADMIN_IDS=("5816048581")
+
+
+
+
+
+
+
+  
